3411df11d4fc5c5d89202d0fb425d87d82f8715a
josuebarriosm <josue.barrios@systemlab.com.pe>
2018-05-23 23:13:15 -0500

    Update

--- highperformance/app/Http/Controllers/ReportController.php
+++ highperformance/app/Http/Controllers/ReportController.php
@@ -205,9 +205,9 @@ class ReportController extends Controller {
 
         $chart = (object)array();
         $chart->type = 'radar';
-        $chart->options = (object)array();
-        $chart->options->responsive = true;
-        $chart->options->maintainAspectRatio = false;
+//        $chart->options = (object)array();
+//        $chart->options->responsive = true;
+//        $chart->options->maintainAspectRatio = false;
         if(is_null($percentageStartTypeArrayData) && is_null($percentageFieldZoneArrayData) && is_null($percentageInitialPenetrationArrayData) &&is_null($percentagePlayerPositionArrayData) ){
             $chart->data = null;
         }else{
